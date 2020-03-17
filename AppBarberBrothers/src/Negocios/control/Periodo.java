21352f66e62fbf1ee9599efb558cab75fa7a607a
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-03-17 01:00:25 -0700

    Periodo class added

--- AppBarberBrothers/src/Negocios/control/Periodo.java
+++ AppBarberBrothers/src/Negocios/control/Periodo.java
@@ -20,23 +20,24 @@ public class Periodo {
 
     private Periodo() {
     }
-    
+
     /**
-     * Comprueba si existe cupo en determinada fecha y hora, busca las citas 
+     * Comprueba si existe cupo en determinada fecha y hora, busca las citas
      * registradas y calcula el inicio y el final de la fecha para la cita a
      * añadir para comparar si no tiene conflicto con una cita agendada.
-     * 
+     *
      * @param nueva_cita cita a agregar en el calendario
      * @return true si es posible agregar la cita, false en caso contrario
      */
     static boolean comprobarCupo(List lista, Citas nueva_cita) {
+        Date inicio_cita = nueva_cita.getFecha();
+        Date fin_cita = DateUtils.addMinutes(inicio_cita, nueva_cita.getServicio().getDuracion());
+        
         for (Iterator iterator = lista.iterator(); iterator.hasNext();) {
-            final Citas cita = (Citas) iterator.next();
-            final Date inicio_servicio = cita.getFecha();
-            final Date fin_servicio = DateUtils.addHours(cita.getFecha(), cita.getServicio().getDuracion());
-            final Date inicio_cita = cita.getFecha();
-            final Date fin_cita = DateUtils.addHours(inicio_cita, nueva_cita.getServicio().getDuracion());
-            
+            Citas cita = (Citas) iterator.next();
+            Date inicio_servicio = cita.getFecha();
+            Date fin_servicio = DateUtils.addMinutes(cita.getFecha(), cita.getServicio().getDuracion());
+
             boolean esta_ocupando = (DateUtils.truncatedCompareTo(inicio_cita, inicio_servicio, Calendar.MINUTE) >= 0
                     && DateUtils.truncatedCompareTo(inicio_cita, fin_servicio, Calendar.MINUTE) < 0)
                     || (DateUtils.truncatedCompareTo(fin_cita, inicio_servicio, Calendar.MINUTE) > 0
@@ -51,5 +52,5 @@ public class Periodo {
         }
         return true;
     }
-    
+
 }
