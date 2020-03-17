5d6e8ba1bf8a35cff2ce0293e8f31f3988afc568
lgarcia99 <54911648+lgarcia99@users.noreply.github.com>
2020-03-16 22:36:47 -0700

    añadir función para comprobar cupo

--- AppBarberBrothers/src/Negocios/control/ControlCitas.java
+++ AppBarberBrothers/src/Negocios/control/ControlCitas.java
@@ -5,9 +5,12 @@ import Datos.entidades.Citas;
 import Datos.entidades.Clientes;
 import Datos.entidades.Empleado;
 import Datos.entidades.Servicios;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import org.apache.commons.lang3.time.DateUtils;
 
 
 public class ControlCitas extends Control {
@@ -19,8 +22,12 @@ public class ControlCitas extends Control {
     }
     
     public void agregar(int id, Date fecha, Empleado barbero, Clientes cliente, Servicios servicio) {
-        Citas cita = new Citas(id, fecha, barbero, cliente, servicio);        
-        modelo.agregarCita(cita);
+        Citas cita = new Citas(id, fecha, barbero, cliente, servicio);
+        if (comprobarCupo(cita)) {
+            modelo.agregarCita(cita);
+        } else {
+            // TODO: en caso contrario, mostrar mensaje de error al usuario
+        }
     }
     
     public void eliminar(int id) {
@@ -37,6 +44,32 @@ public class ControlCitas extends Control {
     
     public List listar() {
         return modelo.listarCitas();
+    }
+    
+    /**
+     * Comprueba si existe cupo en determinada fecha y hora, busca las citas 
+     * registradas y calcula el inicio y el final de cada fecha para comparar si 
+     * tiene conflicto con una cita agendada.
+     * 
+     * @param nueva_cita cita a agregar en el calendario
+     * @return true si es posible agregar la cita, false en caso contrario
+     */
+    public boolean comprobarCupo(Citas nueva_cita) {
+        for (Iterator iterator = listar().iterator(); iterator.hasNext();) {
+            Citas cita = (Citas) iterator.next();
+            Date inicio_servicio = cita.getFecha();
+            Date fin_servicio = DateUtils.addHours(cita.getFecha(), cita.getServicio().getDuracion());
+            
+            try {
+                if (DateUtils.truncatedCompareTo(inicio_servicio, cita.getFecha(), Calendar.MINUTE) <= 0
+                        && DateUtils.truncatedCompareTo(fin_servicio, cita.getFecha(), Calendar.MINUTE) >= 0) {
+                    throw new Exception("Hay una cita agendada a la misma hora.");
+                }
+            } catch (Exception e) {
+                return false;
+            }
+        }
+        return true;
     }
     
 }
